e2ac6a1b1a54dc93b527ffe3215ddb3368ae87ba
ankitmehrotrachemistry <ankitmehrotrachemistry@gmail.com>
2022-10-12 16:14:41 +0530

    Message

---
+++ 5.1). Binary Tree/BinaryTree.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+   public :
+      int data;
+      Node* left;
+      Node* right;
+
+    Node(int d)
+    {
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
+};
+
+Node* buildTree(Node* root)
+{
+    cout << "Enter the Data : " << endl;
+    int data;
+    cin >> data;
+    root = new Node(data);
+
+    if(data == -1)
+    {
+        return NULL;
+    }
+    cout << "Enter Data in left of " << data << endl;
+    root -> left = buildTree(root -> left);
+
+    cout << "Enter Data in right of " << data << endl;
+    root -> right = buildTree(root -> right);
+
+    return root;
+}
+
+int main()
+{
+    Node* root = NULL;
+
+    // Create a Tree
+    root = buildTree(root);
+
+    return 0;
+}
