3d6f06d0af7c637a4dcce7e75659552ae3d1b63d
Ankit Mehrotra <64391917+ankitmehrotrachemistry@users.noreply.github.com>
2023-04-02 19:15:19 +0530

    Add files via upload

---
+++ 2.1). Array/Array_Contiguous_SubArray/Array_Contiguous_SubArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Contiguous_SubArray
+{
+    internal class Program
+    {
+        public static int maximumSubArraySum(int[] array, int size)
+        {
+            int maxSoFar = 0, maxEndHere = 0;
+
+            for(int i = 0; i < size; i++)
+            {
+                maxEndHere = maxEndHere + array[i];
+                if(maxEndHere < 0)
+                {
+                    maxEndHere = 0;
+                }
+                if(maxSoFar < maxEndHere)
+                {
+                    maxSoFar = maxEndHere;
+                }
+            }
+            return maxSoFar;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, -2, 5, 3, 4, -4, 6, -5, 2, 1, 0};
+
+            Console.WriteLine("Maximum Sub Array Sum is : " + maximumSubArraySum(array, 11));
+        }
+    }
+}
